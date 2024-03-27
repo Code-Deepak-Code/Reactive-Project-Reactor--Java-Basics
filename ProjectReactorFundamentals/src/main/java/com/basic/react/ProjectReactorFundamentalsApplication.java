314a857053ff42de40d55fc190b093d09ef2765a
Vivid-Vortex <ctntc9.dpk@gmail.com>
2024-03-27 14:56:01 +0530

    Initial commit

---
+++ ProjectReactorFundamentals/src/main/java/com/basic/react/ProjectReactorFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.basic.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectReactorFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectReactorFundamentalsApplication.class, args);
+	}
+
+}
---
+++ ProjectReactorFundamentals/src/test/java/com/basic/react/ProjectReactorFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectReactorFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
